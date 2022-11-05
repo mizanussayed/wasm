26aa1ec3beddd6bb36825b2bb693b06c0fafff2c
mizanussayed <mrahmandusp@gmail.com>
2022-11-05 23:23:31 +0600

    Initial Commit

---
+++ Models/Member.cs
@@ -0,0 +1,14 @@
+namespace client.Models;
+public class Member 
+    {
+        public int Id {get;set;}
+        public string Name { get; set; }
+        public string BloodGroup { get; set; }
+        public string RoomNumber { get; set; }
+        public string Block { get; set; }
+        public string Session { get; set;}
+        public string Year { get; set; }
+        public string Department { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Picture { get; set; }
+    }
---
+++ Services/IRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace client.Services;
+
+public interface IRepository<T>
+{
+    public Task<IEnumerable<T>> GetAll();
+    public Task<T> GetOne(int Id);
+    public Task<T> Insert(T model);
+    public Task Update(T model);
+    public Task Delete(int Id);
+}
+
+
